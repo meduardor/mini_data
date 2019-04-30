a38ee5f3c4c37a67e62cce382d787b86fe0a9f09
Marco Eduardo <meduardo.r@gmail.com>
2019-04-30 00:50:05 -0300

    Saida dos dados do arquivo .json, com status de error

--- src/main.py
+++ src/main.py
@@ -2,18 +2,22 @@
 
 
 import writer
-import table
+import read
 
-
-dataName = input('Nome do Banco: ')
+nameEx = input('Qual extensão json/csv: ').upper()
+dataName = input('Nome do Banco: ').upper()
 dataKeys = input('Nome da Chave: ')
 dataValues = input('Nome dos Valores: ')
 
 
 if __name__ == '__main__':
-    bancoData = writer.documentJson(dataName)
-    bancoProduct = writer.writerData(dataKeys, dataValues)
-    res = bancoProduct.table.rowsKeys()
-    print(bancoData, bancoProduct)
-    print(res)
 
+    if nameEx == "JSON":
+        bancoData = writer.documentJson(dataName)
+        bancoProduct = writer.writerDatajson(dataKeys, dataValues, dataName)
+        readname = read.ReadDataJson(bancoData)
+    elif nameEx == "CSV":
+        pass
+        # res = bancoProduct.table.rowsKeys()
+    print(bancoData, bancoProduct)
+    # print(res)
