1589624903d5d3dbf166d53ca3679ce2e6ec1853
Marco Eduardo <meduardo.r@gmail.com>
2019-05-10 21:41:42 -0300

    Incrementando a func de escrita do arquivo csv

--- src/writer.py
+++ src/writer.py
@@ -3,15 +3,7 @@
 
 import os
 import json
-
-
-def documentJson(filename):
-    caminho = '.dataset/'
-    arquivo = caminho + (filename + ".json")
-    if not os.path.exists(caminho):
-        os.makedirs(caminho)
-    if not os.path.exists(arquivo):
-        open(arquivo)
+import csv
 
 
 def documentCsv(filename):
@@ -23,19 +15,20 @@ def documentCsv(filename):
         open(arquivo)
 
 
-def pairwise(iterable):
-    it = iter(iterable)
-    return zip(it, it)
+def writerDatajson(filename):
+    lista_key = []
+    lista_value = []
+    count = int(input('Digite o numero de Dados: '))
+    # Dados que serão trasmitidos para o arquivo
+    # Incluindo dados na lista.
+    for i in range(count):
+        lista_key.append(input("Digite uma Chave: "))
 
+    for x in range(count):
+        lista_value.append(input("Digite um Valor: "))
 
-def writerDatajson(filename, count):
-    lista_dados = []
-# Dados que serão trasmitidos para o arquivo
-    key = input("Digite uma Chave: ")
-    lista_dados.append(key)
-    value = input("Digite um Valor: ")
-    lista_dados.append(value)
-# Criação do Arquivo que vai receber os dados
+    # Criação do Arquivo que vai receber os dados
+
     caminho = '.dataset/'
     arquivo = caminho + (filename + ".json")
     if not os.path.exists(caminho):
@@ -43,9 +36,8 @@ def writerDatajson(filename, count):
     if not os.path.exists(arquivo):
         open(arquivo, 'w')
 
-    # TODO: Criar uma lista unica, fazer um split para converter em um dict
-    lista_key = [lista_dados[0]]
-    lista_value = [lista_dados[1]]
+    # Conversão das listas em dict, para a escrita no documento ".json"
+
     lista_salvar = [dict(zip(lista_key, lista_value))]
     dictData = {"Dados": lista_salvar}
     dictData = json.dumps(dictData, indent=4, sort_keys=False)
@@ -61,6 +53,36 @@ def writerDatajson(filename, count):
                                                                Error))
 
 
-def writerDataCsv(filename, **data):
-    # TODO: Escrever função para gravar dados em arquivos csv
-    pass
+def writerDataCsv(filename):
+    count = int(input('Digite numero de campos: '))
+    fieldnames = []
+    fieldcampos = []
+
+    for i in range(count):
+        fieldnames.append(input('Digite o Nome do campos: '))
+    for x in range(count):
+        fieldcampos.append(input('Digite os Campos: '))
+
+    lista_salvar = dict(zip(fieldnames, ' '))
+    # TODO: Mudar a forma de atrelar os valores no dict
+    # para enviar para o documento
+    lista_salvar1 = dict(zip(' ', fieldcampos))
+# Criação dos arquivos csv
+    caminho = '.dataset/csv/'
+    arquivo = caminho + (filename + ".csv")
+    if not os.path.exists(caminho):
+        os.makedirs(caminho)
+    if not os.path.exists(arquivo):
+        open(arquivo, 'w')
+# TODO: Escrever a primeira fileira de descrição das informações
+# TODO: Depois append dos dados nas informações
+
+    with open(arquivo, 'w+', newline='') as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerow(lista_salvar)
+        with open(arquivo, 'a', newline='') as csv_file:
+            writer = csv.DictWriter(csv_file,
+                                    fieldnames=fieldnames,
+                                    delimiter='\t')
+            writer.writerow(lista_salvar1)
