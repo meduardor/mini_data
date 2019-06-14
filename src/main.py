7db0cd0aa86a19c7712281883611a7da9a592f34
Marco Eduardo <meduardo.r@gmail.com>
2019-06-14 17:48:04 -0300

    Loop e condicional para processamento e armazenametos dos dados na lista

--- src/main.py
+++ src/main.py
@@ -10,25 +10,46 @@ dataName = input('Nome do Banco\n > ').upper()
 count = int(input('Quantidade de dados.\n > '))
 
 # TODO: Definir funções de saida para cada tarefa
+# TODO: Criar uma Chave primaria para identificar os dados.
 
 
 def escrever_json(name, data):
     # TODO: Refatorar essa função de acordo com as novas funcionalidades do
     # NOTE: Exemplo de dados de entrada.(Não permanentes)
-    # Dados constantes "keys"
-    data_json_key = [
 
-        'Nome:',
-        'Data:',
-        'Senha:',
-        'Pergunta:'
-    ]
-    # Dados variaveis entrada do usuario  
-    data_json_value = [data]
-    # Chamada do Modulo writer e das funções de escritas Json  
+    # Criação da base de dados in JSON
     escrita_json = WriterJson.DocJson(name)
-    dados_json = WriterJson.DataJson(data_json_key, data_json_value)
-    escrita_dados_json = WriterJson.ConvertJson(escrita_json, dados_json)
+
+    # Número definido para entrada de e dados
+    count = int(input('Quantos dados vai escrever: '))
+
+    # Chamada do Modulo writer e das funções de escritas Json
+    dados_json_key = WriterJson()
+    dados_json_value = WriterJson()
+
+    # TODO: Fazer testes sobre a entrada de dados das lista.
+    # Listas de dados
+    data_json_value = []
+    data_json_key = []
+
+    # Condicional de entrada de dados
+    if not data_json_key == 0:
+        for i in range(count):
+            data_json_key.append(input('Digite nome dos Campos: '))
+            dados_json_key.DataJson(data_json_key, data)
+        else:
+            pass
+    elif not data_json_value == 0:
+        for x in range(count):
+            data_json_value.append(input('Digite os valores: '))
+            dados_json_value.DataJson(data_json_value, data)
+        else:
+            pass
+
+    # Converte os dos da lista em Dict e grava no arquivo JSON
+    escrita_dados_json = WriterJson.ConvertJson(escrita_json,
+                                                dados_json_key,
+                                                dados_json_value)
     return escrita_dados_json
 
 
