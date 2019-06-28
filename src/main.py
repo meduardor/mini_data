69e3c2055b36eba8abbbf10adb3de75b70ac4ebd
Marco Eduardo <meduardo.r@gmail.com>
2019-06-28 19:49:09 -0300

    Reformulação loop da entrada de dados

--- src/main.py
+++ src/main.py
@@ -10,44 +10,48 @@ from read import ReadJson
 # TODO: Criar uma Chave primaria para identificar os dados.
 
 
-def escrever_json(name, lista_key, lista_value):
+def escrever_json(arquivo, lista_key, lista_value, *data):
     # TODO: Refatorar essa função de acordo com as novas funcionalidades do
     # Criação da base de dados in JSON
-    escrita_json = WriterJson.DocJson(name)
+    escrita_json = WriterJson.DocJson(arquivo)
 
     # Número definido para entrada de e dados
     count = int(input('Quantos dados vai escrever: '))
+    lista_key = []
+    lista_value = []
 
     # Listas de dados
     data_json_value = WriterJson(lista_key, lista_value)
     data_json_key = WriterJson(lista_key, lista_value)
 
     # Chamada do Modulo writer e das funções de escritas Json
-
     # TODO: Fazer testes sobre a entrada de dados das lista.
-    # Condicional de entrada de dados
+    # condicional de entrada de dados
     # TODO(merds): Modificar o loop de entrada de dados.
-    if data_json_key == 0:
-        if not os.path.exists(name):
-            with open(name, 'r') as f:
-                f.readlines()
-                data_json_key.DataJson(escrita_json,
-                                       data_json_key)
+
+    if lista_key == [] and os.path.exists(escrita_json):
+        with open(escrita_json, 'r+') as f:
+            f.readlines()
+            for element in range(count):
+                lista_key.append(data_json_key.DataJson(escrita_json,
+                                                        data))
     else:
         pass
-    if data_json_value == 0:
-        for x in range(count):
-            # data_json_value.append(input('Digite os valores: '))
-            data_json_value.DataJson(escrita_json, data_json_value)
-            data_json_value.dataJson_value(escrita_json)
+    if lista_value == []:
+        for element in range(count):
+            lista_value.append(
+                data_json_value.DataJson(escrita_json, data_json_value),
+                data_json_value.dataJson_value()
+            )
     else:
         pass
 
     # Converte os dos da lista em Dict e grava no arquivo JSON
-    escrita_dados_json = {}
-    escrita_dados_json = WriterJson.ConvertJson(data_json_key,
-                                                data_json_value,
-                                                escrita_json)
+    # escrita_dados_json = {}
+    escrita_dados_json = WriterJson(lista_key, lista_value)
+    escrita_dados_json.ConvertJson(data_json_key,
+                                   data_json_value,
+                                   escrita_json)
     return escrita_dados_json
 
 
